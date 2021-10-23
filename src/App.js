badd2af33120bec2943a86900b3dc251f34479ec
Zack Davis <zack.davis88@hotmail.com>
2021-10-23 15:48:40 -0700

    adding more of a PokeDex look-and-feel

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import SearchPage from './components/SearchPage';
-import ProfilePage from './components/ProfilePage';
-import ProfilesContextProvider from './components/ProfilesContextProvider';
+import NotFoundPage from 'components/NotFoundPage';
+import PokemonContextProvider from './components/PokemonContextProvider';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from 'components/Header';
 import './styles.css';
 
 function App() {
   return (
-    <ProfilesContextProvider>
+    <PokemonContextProvider>
       <Router>
+        <Header />
         <Switch>
           <Route path="/" exact>
             <SearchPage />
           </Route>
-          <Route path="/:profileId" exact>
-            <ProfilePage />
-          </Route>
           <Route path="*">
-            <div>Route Not Found</div>
+            <NotFoundPage />
           </Route>
         </Switch>
       </Router>
-    </ProfilesContextProvider>
+    </PokemonContextProvider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,19 +3,25 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const StyledHeader = styled.header`
-  border-bottom: 1px solid #efefef;
+  -webkit-box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.75);
+  box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.75);
   padding: 16px;
+  background-color: #f44336;
 `;
 
 const StyledImage = styled.img`
-  width: 110px;
+  width: 200px;
 `;
 
 export default function Header() {
   return (
     <StyledHeader>
       <Link to="/">
-        <StyledImage src="./logo.svg" alt="match" />
+        <StyledImage
+          src="https://cdn2.bulbagarden.net/upload/4/4b/Pok%C3%A9dex_logo.png"
+          alt="pokedex"
+        />
       </Link>
     </StyledHeader>
   );
