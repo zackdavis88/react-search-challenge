df687f6cc985d092b75a40e133bac0c28cd618d5
Zack Davis <zack.davis88@hotmail.com>
2021-10-23 17:22:08 -0700

    updating utils and adding pokemon font

--- src/utils.js
+++ src/utils.js
@@ -6,34 +6,42 @@ import {
 } from './components/PokemonContextProvider';
 import { apiUrl } from './apiconfig.json';
 
-export const fetchPokemon = async (profileContext, isRefresh = false) => {
+export const formatPokemonNumber = (number) => {
+  if (number >= 100) return `#${number}`;
+
+  let paddedNumber = `00${number}`;
+  paddedNumber = paddedNumber.slice(paddedNumber.length - 3);
+  return `#${paddedNumber}`;
+};
+
+export const fetchPokemon = async (pokemonContext, isRefresh = false) => {
   // for a better feeling experience, lets not fire the request action for refreshes.
   if (!isRefresh) {
-    profileContext.dispatch({ type: POKEMON_REQUESTED });
+    pokemonContext.dispatch({ type: POKEMON_REQUESTED });
   }
-  const response = await fetch(`${apiUrl}/pokemon?limit=12`);
-  if (response.status !== 200) return profileContext.dispatch({ type: POKEMON_FAILURE });
+  const response = await fetch(`${apiUrl}/pokemon?limit=20`);
+  if (response.status !== 200) return pokemonContext.dispatch({ type: POKEMON_FAILURE });
 
   const body = await response.json();
   const results = body.results;
-  console.log(results);
   const pokemonArray = results.map((pokemon, index) => {
     return {
       ...pokemon,
-      photoUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+      imageUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
         index + 1
       }.png`,
+      number: index + 1,
     };
   });
   // Fire off a dispatch to populate or refresh the store.
-  return profileContext.dispatch({
+  return pokemonContext.dispatch({
     type: isRefresh ? REFRESH_POKEMON : POKEMON_SUCCESS,
-    profiles: pokemonArray || [],
+    pokemonArray: pokemonArray || [],
   });
 };
 
-export const sortAscending = (profileArray) =>
-  profileArray.sort((profileA, profileB) => (profileA.name > profileB.name ? 1 : -1));
+export const sortAscending = (pokemonArray) =>
+  pokemonArray.sort((pokemonA, pokemonB) => (pokemonA.name > pokemonB.name ? 1 : -1));
 
-export const sortDescending = (profileArray) =>
-  profileArray.sort((profileA, profileB) => (profileA.name < profileB.name ? 1 : -1));
+export const sortDescending = (pokemonArray) =>
+  pokemonArray.sort((pokemonA, pokemonB) => (pokemonA.name < pokemonB.name ? 1 : -1));
